78c11f7759a7fbc9b3eb9c4095392e78c59c6a9c
Song Gao <song@gao.io>
2013-08-27 16:10:14 -0500

    style changes for toc; add alt for images

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,17 +13,24 @@ Controller.prototype.populate_toc = function() {
     var self = this;
     var populate_obj = function(obj, div) {
         if (obj.children && obj.children[0] && obj.children[0].children) {
-            var child = $('<div></div>');
             var id = obj.id ? obj.id : obj.name;
-            child.append('<a class="accordion-toggle" data-toggle="collapse" href="#' + id + '">' + obj.name + '</a>');
-            var children = $('<div class="indent toc_list collapse" id="' + id + '"></div>');
+            var link = $('<a class="accordion-toggle" data-toggle="collapse" href="#' + id + '"><span>' + obj.name + '</span></a>');
+            var children = $('<div class="toc_list collapse" id="' + id + '"></div>');
+            children.on('shown.bs.collapse', function() {
+                link.addClass('dir-in');
+            });
+            children.on('hidden.bs.collapse', function() {
+                link.removeClass('dir-in');
+            });
             $.each(obj.children, function(index, item) {
                 populate_obj(item, children);
             });
-            child.append(children);
-            div.append(child);
+            var container = $('<div></div>');
+            container.append(link);
+            container.append(children);
+            div.append(container);
         } else {
-            var clickable = $('<a href="#">' + obj.name + '</a><br/>');
+            var clickable = $('<a href="#"><span>' + obj.name + '</span></a><br/>');
             self.bind_click(clickable, obj.children);
             div.append(clickable);
         }
@@ -36,19 +43,29 @@ Controller.prototype.populate_toc = function() {
 Controller.prototype.bind_click = function(clickable, content) {
     var cnt = $('#content');
     clickable.click(function() {
+        $('.link-in').removeClass('link-in');
+        clickable.addClass('link-in');
         cnt.html('');
         $.each(content, function(index, obj) {
             var row = $('<div class="content_item"></div>');
-            row.append('<img class="content_item_img" src="/content/' + obj.filename + '"/>');
+            row.append('<img class="content_item_img" src="/content/' + obj.filename + '" alt="' + obj.name + '"/>');
             cnt.append(row);
         });
     });
 };
 
-$(window).load(function() {
-    ctl = new Controller();
-    ctl.get_structure(function() {
-        ctl.populate_toc();
-        $('#toc > a:first').trigger('click');
+function init() {
+    $(window).resize(function() {
+        $('#toc').height($('.sidebar').height() - $('.sidebar').width() - $('#toc').position().top - parseInt($('.sidebar').css("margin-top")));
     });
-});
+    $(window).load(function() {
+        ctl = new Controller();
+        ctl.get_structure(function() {
+            ctl.populate_toc();
+            $(window).trigger('resize');
+            $('#toc > a:first').trigger('click');
+        });
+    });
+}
+
+init();
